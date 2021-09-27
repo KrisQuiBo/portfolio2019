63afc3f09f9337190c8b84ea9ba9c67f336de859
Kristen Quinlan-Boyce <kqboyce@gmail.com>
2021-09-27 10:05:07 -0400

    major update and reorganization of site content

--- js/main.js
+++ js/main.js
@@ -1,44 +1,67 @@
-const navigation = document.querySelector(".navigation");
-const dropDown = document.getElementById('drop-content');
+
 const drop = document.querySelector('.drop');
 const activeElements = document.querySelectorAll(".active-element");
 const hexag = document.querySelectorAll('.polyg-border');
-let a = document.querySelectorAll('a');
-const modal = document.getElementById('myModal');
-const closeBtn = document.querySelector('span.close');
-let cards = document.querySelectorAll('.container');
-const id= document.getElementById('about');
+let container = document.querySelectorAll('.container');
+let cards = document.querySelectorAll('.card')
+const about = document.getElementById('about');
 var windowheight = window.innerHeight;
 const skills = document.querySelector('.skills');
 var scrollheight = document.body.scrollHeight;
 let skillsHeight = getComputedStyle(skills).height.split('px')[0];
 let hexagArea = document.querySelectorAll(".hexag");
-
-
+let tiltHeadline = document.querySelectorAll('.tiltHeadline');
+const upBtn = document.querySelector('.up');
+const portHead = document.querySelector('.portHeader');
+const closeBtn = document.querySelector('.closeBtn');
 
 window.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM fully loaded and parsed');
 
-  // var toggledMenu = drop.addEventListener("click", function() {
-  //   activeElements.forEach(function(e){
-  //       e.classList.toggle("active");
-  //       console.log('click');
-  //   });
-  // });
+  var toggledMenu = drop.addEventListener("mouseover", function() {
+    activeElements.forEach(function(e){
+        e.classList.add('active');
+    });
+  });
+
+function toggleMenuOff(e) {
+  activeElements.forEach(function(e){
+    e.classList.remove('active');
+  });
+}
+
+closeBtn.addEventListener('click', toggleMenuOff);
+
   hexag.forEach(function(e) {
     e.classList.add('rotate-it');
   });
 
-  function menuFix() {
-    if (document.documentElement.scrollTop > 80 || document.body.scrollTop > 80) {
-      document.getElementById('tilt').classList.remove('tilt');
-      document.getElementById('tilt').classList.add('tiltStart');
-    } else if (document.documentElement.scrollTop < 80 || document.body.scrollTop < 80) {
-        document.getElementById('tilt').classList.add('tilt');
-        document.getElementById('tilt').classList.remove('tiltStart');
-      }
+  // function menuFix() {
+  //   tiltHeadline.forEach(headline => {
+  //   if (document.documentElement.scrollTop > 80 || document.body.scrollTop > 80) {
+  //     tiltHeadline.removeClass('tiltHeadline');
+  //     // tiltHeadline.addClass('tiltStart');
+  //   } else if (document.documentElement.scrollTop < 80 || document.body.scrollTop < 80) {
+  //       document.getElementById('tilt').classList.add('tilt');
+  //       // document.getElementById('tilt').classList.remove('tiltStart');
+  //     }
+  //   });
+  // }
+
+  function revealUpBtn() {
+    let bottomAbout = about.getBoundingClientRect().bottom;
+    if(bottomAbout < 0) {
+      upBtn.classList.add('show');
+    } else {
+      upBtn.classList.remove('show');
+    }
   }
 
+function toggleAngle() {
+    this.classList.toggle('tilt');
+  }
+ tiltHeadline.forEach(tilt => {tilt.addEventListener('mouseover', toggleAngle)});
+ 
   function scrollHighlight() {
     if (document.documentElement.scrollTop > 195 || document.body.scrollTop > 195) {
       document.querySelector('.myName').classList.add('highlightName');
@@ -48,7 +71,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
   }
 
   function scrollBorder() {
-    if (document.documentElement.scrollTop > 1060 || document.body.scrollTop > 1260) {
+    if (document.documentElement.scrollTop > 800 || document.body.scrollTop > 1260) {
       document.querySelector('.skills').classList.add('highlight');
     } else if (document.documentElement.scrollTop < 955 || document.body.scrollTop < 955){
       document.querySelector('.skills').classList.remove('highlight');
@@ -63,97 +86,12 @@ window.addEventListener('DOMContentLoaded', (e) => {
     }
   }
 
-// const revealLinks = (e) => {
-//   for (let i=0; i < hexag.length; i++) {
-//     hexag[i].classList.add("revealLinks");
-//   }
-// }
-// for(let i=0; i<hexagArea.length;i++) {
-// hexagArea[i].addEventListener('mouseover', function(e) {
-//   for(let i=0;i<hexagArea.length;i++) {
-//     hexag[i].classList.add("revealLinks");
-//   // revealLinks();
-//   }
-// });
-// }
-
-
-var f = function(e) {
-
-  // the event target, can be any element in the page at this point
-  var t = e.target;
-
-  // check if the event target is an img element
-  if(t.tagName.toLowerCase() == 'img') {
-
-    // then toggle its active class
-    t.classList.toggle('revealLinks');
-  }
-}
-
-// // add listeners to the window (or on whatever image container you have)
-// window.addEventListener('mouseover', f /* call this function f on mouseover */, false);
-// window.addEventListener('mouseout', f, false);
-//
-// for(let i=0; i<cards.length;i++) {
-//   cards[i].addEventListener('mouseover', () => {
-//     let visLink = document.querySelectorAll('repoLink');
-//     visLink[0].classList.toggle('showLink');
-//   })
-// }
-
-
-// for(let i=0; i<cards.length; i++) {
-//   cards[i].addEventListener('mouseover', ()=> {
-//     let addLinks = "";
-//     addLinks +=
-//     `<p class="repoLink">
-//       <a href="https://github.com/KrisQuiBo/portfolio2019/tree/gh-pages">Take me to gitHub repo</a><br><br>
-//       <a href="https://krisquibo.github.io/web_app_dashboard_v3.1/">Take me to example</a>
-//     </p>`;
-//     hexagArea.innerHTML = addLinks;
-//   });
-// }
-//
-// var f = function(e) {
-//
-//   // the event target, can be any element in the page at this point
-//   var t = e.target;
-//
-//   // check if the event target is an img element
-//   if(t.className.toLowerCase() == 'polygBorder') {
-//
-//     // then toggle its active class
-//     t.classList.toggle('revealLinks');
-//   }
-// }
-
-// add listeners to the window (or on whatever image container you have)
-window.addEventListener('mouseover', f /* call this function f on mouseover */, false);
-window.addEventListener('mouseout', f, false);
-
-
-// hexagArea.addEventListener('mouseout', function() {
-//   hexag[i].classList.remove("revealLinks");
-// })
-
-  // function scrollSkills() {
-  //   // let skillsHeight = getComputedStyle(header).height.split('px')[0];
-  //   if(skillsHeight + (window.pageYOffset  - skillsHeight) == 0) {
-  //     skills.classList.add('.highlight');
-  //   }
-  // }
-  //
-  // window.onscroll = function() {
-  //   scrollSkills();
-  //   scrollEmbellish()
-  // }
-
-  window.onscroll = function() {
+window.onscroll = function() {
     scrollHighlight();
     scrollBorder();
     scrollSlide();
-    menuFix();
+    revealUpBtn();
+    // menuFix();
   };
 
 
